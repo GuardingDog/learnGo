67d953b8b48a88dd75867d847162d9a46cb82fc3
SunQingzhi <sun2387353@163.com>
2023-09-01 22:54:55 +0800

    [ADD] 添加TODO项

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 - [ ] 何为FirstClass?
 - [ ] Simplify error handling: [blog](https://go.dev/blog/error-handling-and-go)
 - [ ] Reflect Three Point[blog](https://go.dev/blog/laws-of-reflection)
+- [ ] Select Case 的详细使用[blog](https://go.dev/blog/concurrency-timeouts)
+- [ ] GNC Debugger 的详细使用[blog](https://go.dev/blog/debug-status)
+- [ ] Go 中String, Byte, Rune, utf-8, unicode等梳理
+- [ ] 文本正常化中的NFC, NFD, NFKC, NFKD的概念[blog](https://go.dev/blog/normalization)
+- [ ] Go Test 的使用与实现原理[blog](https://go.dev/blog/cover)
+- [ ] Go PlayGround的实现架构原理[blog](https://go.dev/blog/playground)
--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,74 @@ package main
 
 import (
 	"fmt"
-	"learngo/pkg/data"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"os"
 )
 
 func main() {
-	fmt.Println("GobDemo ...")
-	data.GobDemo()
+	// 创建一个新的RGBA图像对象
+	width := 200
+	height := 200
+	rgba := image.NewRGBA(image.Rect(0, 0, width, height))
 
-	fmt.Println("BasicDemo ...")
-	data.BasicDemo()
+	// 填充背景色
+	bgColor := color.RGBA{255, 255, 255, 255}
+	draw.Draw(rgba, rgba.Bounds(), &image.Uniform{bgColor}, image.Point{}, draw.Src)
 
-	fmt.Println("EncodeDecodeDemo ...")
-	data.EncodeDecodeDemo()
+	// 绘制笑脸
+	centerX := width / 2
+	centerY := height / 2
+	radius := width / 4
+	drawCircle(rgba, centerX, centerY, radius, color.White)
 
-	fmt.Println("InterfaceDemo ...")
-	data.InterfaceDemo()
+	eyeRadius := radius / 8
+	drawCircle(rgba, centerX-radius/2, centerY-radius/2, eyeRadius, color.Black)
+	drawCircle(rgba, centerX+radius/2, centerY-radius/2, eyeRadius, color.Black)
+
+	mouthWidth := radius / 2
+	mouthHeight := radius / 4
+	mouthX := centerX - mouthWidth/2
+	mouthY := centerY + radius/2 - mouthHeight/2
+	drawEllipse(rgba, mouthX, mouthY, mouthWidth, mouthHeight, color.Black)
+
+	// 保存图像到文件
+	outputFile, err := os.Create("smiley_face.png")
+	if err != nil {
+		fmt.Println("Failed to create output file:", err)
+		return
+	}
+	defer outputFile.Close()
+
+	err = png.Encode(outputFile, rgba)
+	if err != nil {
+		fmt.Println("Failed to encode image:", err)
+		return
+	}
+
+	fmt.Println("Smiley face generated!")
+}
+
+// 绘制圆形
+func drawCircle(img draw.Image, centerX, centerY, radius int, color color.Color) {
+	for x := centerX - radius; x <= centerX+radius; x++ {
+		for y := centerY - radius; y <= centerY+radius; y++ {
+			if (x-centerX)*(x-centerX)+(y-centerY)*(y-centerY) <= radius*radius {
+				img.Set(x, y, color)
+			}
+		}
+	}
+}
+
+// 绘制椭圆
+func drawEllipse(img draw.Image, x, y, width, height int, color color.Color) {
+	for cx := x - width/2; cx <= x+width/2; cx++ {
+		for cy := y - height/2; cy <= y+height/2; cy++ {
+			if (cx-x)*(cx-x)*height*height+(cy-y)*(cy-y)*width*width <= height*height*width*width {
+				img.Set(cx, cy, color)
+			}
+		}
+	}
 }
